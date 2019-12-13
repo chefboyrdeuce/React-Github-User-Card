253b7839b52e6799e5bd95f89ae47d1bd1eb67f2
chefboyrdeuce <jpldeuce@gmail.com>
2019-12-13 18:44:52 -0500

    completed MVP

--- github-user-card/src/components/UserCard.js
+++ github-user-card/src/components/UserCard.js
@@ -2,24 +2,45 @@ import React from 'react';
 import axios from "axios";
 
 class UserCard extends React.Component {
-    render(){
-        return{
-       
+  constructor() {
+    super();
 
-        }
-        
+    this.state = {
+      userCard: []
+    }
+
+    console.log(this.state)
+  }
+
+  componentDidMount(){
+    axios.get('https://api.github.com/users/chefboyrdeuce/followers') 
+      .then(response => {
+        this.setState({
+          userCard: response.data
+      })
+        console.log(response.data)
+      })
+      .catch(error => {
+        console.error(error)``
+      })
+  }
+
+    render(){
+        return(
+          this.state.userCard.map( (follower)=>{
+            
+            return  (        
+            <div className="card">
+              <img src= {follower.avatar_url}></img>
+              <div className="card-info">
+                <h3 className="username">{follower.login}</h3>
+                <p className="username"> {follower.username}</p>
+              </div>
+            </div>
+          )})
+        )
     }
 }
 
 export default UserCard;
 
-
-axios
-
-.get('https://api.github.com/users/chefboyrdeuce/followers') //function called get accepts url argument
-.then(response => {
-  console.log(response);
-})
-.catch(error => {
-  console.error(error)
-})
